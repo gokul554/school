1cf4d8c64a00bb62c442c7fbdaaf3386b5f25980
Gokul <gokulmanikonda@gmail.com>
2020-07-31 12:35:04 +0100

    update htm page with we elements

--- src/test/java/pages/HTMLPage.java
+++ src/test/java/pages/HTMLPage.java
@@ -1,8 +1,15 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HTMLPage {
   
-
+	@FindBy(xpath=".//*[contains(text(),'Java Intro')]")
+	private WebElement JavaIntroTab;
+	
+	@FindBy(xpath=".//*[contains(text(),'Java Syntax')]")
+	private WebElement JavaSyntaxTab;
 
 }
 
