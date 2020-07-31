6a01bbc5c1c0bfd30cad1778b070f3b6ac91014c
Gokul <gokulmanikonda@gmail.com>
2020-07-31 12:33:25 +0100

    updated html

--- src/test/java/pages/HTMLPage.java
+++ src/test/java/pages/HTMLPage.java
@@ -2,12 +2,7 @@ package pages;
 
 public class HTMLPage {
   
-<<<<<<< HEAD
-	
-=======
-  @FindBy(xpath=".//input[@name='email']")
-	private WebElement txtEmail;
->>>>>>> ce54f7aecd595b1a5ab54942ed331a52063683e9
+
 
 }
 
